98031c7d4c668ae99f77888beed3707baa3270dc
Erik Basargin <basargin.erik@gmail.com>
2023-06-20 21:01:20 +0100

    FB12383213: SwiftCompile crashes when using parameter packs with `async let`

--- Sources/AsyncZip/AsyncZip.swift
+++ Sources/AsyncZip/AsyncZip.swift
@@ -1,9 +1,12 @@
 import Foundation
 
+// FB12383213: SwiftCompile crashes when using parameter packs
 func zip<FirstElement, SecondElement, each Element>(
     _ firstOperation: @Sendable @escaping () async throws -> FirstElement,
     _ secondOperation: @Sendable @escaping () async throws -> SecondElement,
     _ operation: repeat @Sendable @escaping () async throws -> (each Element)) async throws -> (FirstElement, SecondElement, repeat each Element)
 {
-    try await (firstOperation(), secondOperation(), repeat (each operation)())
+    async let results = (firstOperation(), secondOperation(), repeat (each operation)())
+    
+    return try await results
 }
