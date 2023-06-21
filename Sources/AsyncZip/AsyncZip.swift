59cfab9b5e66596875fa4b6cb33f1a9ec7dcaee3
Erik Basargin <basargin.erik@gmail.com>
2023-06-21 21:43:29 +0100

    FB12410364: Swift Compiler crash: 'Stored value type does not match pointer operand type!'
    Version 1 fixed from Xcode 15 beta 4

--- Sources/AsyncZip/AsyncZip.swift
+++ Sources/AsyncZip/AsyncZip.swift
@@ -5,61 +5,45 @@ func zip<FirstElement, SecondElement, each Element>(
     _ secondOperation: @Sendable @escaping () async throws -> SecondElement,
     _ operation: repeat @Sendable @escaping () async throws -> (each Element)) async throws -> (FirstElement, SecondElement, repeat each Element)
 {
+    let taskStorage = TaskStorage()
+    
+    // Version 1. Swift Compiler crash: 'Stored value type does not match pointer operand type!'
+    // Fixed from Xcode 15 beta 4
+    func bind<each T>(
+        operation: repeat @Sendable @escaping () async throws -> (each T),
+        with storage: TaskStorage) async
+    {
+        _ = (repeat await storage.push(each operation))
+    }
+    
+    await (repeat bind(operation: each operation, with: taskStorage))
+    
+    // Version 2. Replace lines 11-18 with the following to see another crash.
+    // await Task { [taskStorage] in
+    //     _ = (repeat await taskStorage.push(each operation))
+    // }.value
+    
     async let firstOperation = try firstOperation()
     async let secondOperation = try secondOperation()
     
     return (
         try await firstOperation,
         try await secondOperation,
-        try await repeat task(each operation)
+        repeat try await taskStorage.pop((each Element).self)!
     )
 }
 
-protocol ElementIDProtocol {
-    associatedtype ID
-    associatedtype Value
+private actor TaskStorage {
     
-    var id: ID { get }
-    var valueType: Value.Type { get }
-}
-
-struct ElementID<Value> {
-    let id = UUID()
-    let valueType: Value.Type
-}
-
-private func task<each Element>(
-    _ operation: repeat @Sendable @escaping () async throws -> (each Element)) async throws -> (repeat each Element)
-{
-    var storage: [(() async throws -> Any)] = []
-    func makeTask<T>(_ operation: @Sendable @escaping () async throws -> T) {
-        let task = Task(operation: operation)
-        storage.append({ try await task.value })
+    private var storage: [Task<Any, Error>] = []
+    
+    func push<T>(_ operation: @Sendable @escaping () async throws -> T) {
+        let task = Task(operation: operation) as Task<Any, Error>
+        storage = [task] + storage
     }
     
-    _ = (repeat makeTask(each operation))
-    
-//    print("[task] storage size \(storage.count)") // appears that storage would never be more than 1
-//    
-//    return try await (repeat unwrapElement(each operation, storage: storage))
-    
-    let storageTwo = storage
-    // seems like Swift compiler does not like this part
-    return try await Task { try await (repeat unwrapElement(each operation, storage: storageTwo)) }.value
-}
-
-func unwrapElement<each Element>(
-    _ operation: repeat @Sendable @escaping () async throws -> (each Element),
-    storage: [(() async throws -> Any)]) async throws -> (repeat each Element)
-{
-    print("[unwrap] storage size: \(storage.count)")
-    
-    var index: Int = 0
-    func unwrap<T>(_ operation: @Sendable @escaping () async throws -> T) async throws -> T {
-        print("[unwrap] Index \(index)")
-        defer { index += 1 }
-        return (try await storage[index]()) as! T
+    func pop<T>(_ key: T.Type) async throws -> T? {
+        guard let task = storage.popLast() else { return nil }
+        return try await task.value as? T
     }
-    
-    return try await (repeat unwrap(each operation))
 }
