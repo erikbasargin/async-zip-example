36413da0590cf2b97cf93e0460b1127be3ce89d3
Erik Basargin <basargin.erik@gmail.com>
2023-06-24 15:18:39 +0100

    First green test :)

--- Sources/AsyncZip/AsyncZip.swift
+++ Sources/AsyncZip/AsyncZip.swift
@@ -7,33 +7,18 @@ func zip<FirstElement, SecondElement, each Element>(
 {
     let taskStorage = TaskStorage()
     
-    // Version 1. Swift Compiler crash: 'Stored value type does not match pointer operand type!'
-    // Fixed from Xcode 15 beta 4
-    func bind<each T>(
-        operation: repeat @Sendable @escaping () async throws -> (each T),
-        with storage: TaskStorage) async
-    {
-        _ = (repeat await storage.push(each operation))
-    }
+    taskStorage.push(firstOperation)
+    taskStorage.push(secondOperation)
+    (repeat taskStorage.push(each operation))
     
-    await (repeat bind(operation: each operation, with: taskStorage))
-    
-    // Version 2. Replace lines 11-18 with the following to see another crash.
-    // await Task { [taskStorage] in
-    //     _ = (repeat await taskStorage.push(each operation))
-    // }.value
-    
-    async let firstOperation = try firstOperation()
-    async let secondOperation = try secondOperation()
-    
-    return (
-        try await firstOperation,
-        try await secondOperation,
-        repeat try await taskStorage.pop((each Element).self)!
+    return try await (
+        taskStorage.pop(FirstElement.self)!,
+        taskStorage.pop(SecondElement.self)!,
+        repeat taskStorage.pop((each Element).self)!
     )
 }
 
-private actor TaskStorage {
+private final class TaskStorage {
     
     private var storage: [Task<Any, Error>] = []
     
